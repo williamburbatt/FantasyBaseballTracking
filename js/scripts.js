990aa3b4f9b82eb17f4454f7a930d6a494db65d1
williamburbatt <williamburbatt@yahoo.com>
2019-02-06 13:07:15 -0600

    started hashing out front end part of website.

---
+++ js/scripts.js
@@ -0,0 +1,133 @@
+
+
+function baseballAPI() {
+    var user = config.apiKey;
+    var pass = config.pass;
+    var encrypted_api_key_credentials = "" + user + ":" + pass;
+
+
+
+    const app = document.getElementById('root');
+
+    const logo = document.createElement('img');
+    logo.src = 'images/logo.png';
+
+    const container = document.createElement('div');
+    container.setAttribute('class', 'container');
+
+
+    app.appendChild(logo);
+    app.appendChild(container);
+
+
+
+    var request = new XMLHttpRequest();
+    request.open('GET', 'https://api.mysportsfeeds.com/v2.0/pull/mlb/2018-regular/player_stats_totals.json', true);
+    request.setRequestHeader("Authorization", "Basic " + btoa(user + ":" + pass));
+    request.onload = function () {
+        // Begin accessing JSON data here
+
+        var data = JSON.parse(this.response);
+
+
+
+
+        if (request.status >= 200 && request.status < 400) {
+            var players = data.playerStatsTotals;
+            players.forEach(player => {
+
+                var p1 = {
+                    fName: player.player.firstName,
+                    lName: player.player.lastName,
+                    id: player.player.id,
+                    pos: player.player.primaryPosition,
+                    team: player.team.abbreviation,
+                    fullName: function () {
+                        return "" + this.pos + " " + this.fName + " " + this.lName + " " + this.team;
+                    },
+                    stats: function () {
+                        var batting = player["stats"]["batting"];
+                        var currentRow = "<tr>";
+                        var finalTable = "";
+                        var count = 0;
+                        for (var key in batting) {
+                            count++;
+                            if (batting.hasOwnProperty(key)) {
+                                currentRow = currentRow + "<td>" + (key + " : " + batting[key]) + "</td>";
+                            }
+                            if (count % 2 == 0) {
+                                finalTable = finalTable + currentRow + "</tr>";
+                                currentRow = "<tr>";
+                            }
+                        }
+
+                        return finalTable;
+
+                    },
+
+                    generalBattingStats: function () {
+                        var batting = player["stats"]["batting"];
+                        var runs = batting.runs;
+                        var homeruns = batting.homeruns;
+                        var rbi = batting.runsBattedIn;
+                        var sb = batting.stolenBases;
+                        var avg = batting.battingAvg;
+                        var finalTable = "<tr><td><b>runs:</b></td><td>" + runs + "</td></tr>";
+                        finalTable = finalTable + "<tr><td><b>homeruns:</b></td><td>" + homeruns + "</td></tr>";
+                        finalTable = finalTable + "<tr><td><b>rbi:</b></td><td>" + rbi + "</td></tr>";
+                        finalTable = finalTable + "<tr><td><b>sb:</b></td><td>" + sb + "</td></tr>";
+                        finalTable = finalTable + "<tr><td><b>avg:</b></td><td>" + avg + "</td></tr>";
+                        return finalTable
+                    },
+                    generalPitchingStats: function () {
+                         var pitching = player["stats"]["pitching"];
+                          var wins = pitching.wins;
+                          var era = pitching.earnedRunAvg;
+                          var ks = pitching.pitcherStrikeouts;
+                          var whip = pitching.walksAndHitsPerInningPitched;
+                          var saves = pitching.saves;
+                          var finalTable ="<tr><td><b>wins:</b></td><td>" + wins + "</td></tr>";
+                          finalTable = finalTable + "<tr><td><b>era:</b></td><td>" + era + "</td></tr>";
+                          finalTable = finalTable + "<tr><td><b>ks:</b></td><td>" + ks + "</td></tr>";
+                          finalTable = finalTable + "<tr><td><b>whip:</b></td><td>" + whip + "</td></tr>";
+                          finalTable = finalTable + "<tr><td><b>saves:</b></td><td>" + saves + "</td></tr>";
+
+                        return finalTable
+                    }
+
+                };
+
+
+                const card = document.createElement('div');
+                card.setAttribute('class', 'card');
+
+                const h1 = document.createElement('h1');
+                h1.setAttribute('class', p1.team.toLowerCase());
+                h1.textContent = p1.fullName();
+
+                const p = document.createElement('table');
+                var stats;
+                var test = ""+p1.pos;
+
+                if (test == "P") {
+                    stats = p1.generalPitchingStats();
+                } else {
+                    stats = p1.generalBattingStats();
+                }
+
+                p.innerHTML = stats;
+                container.appendChild(card);
+                card.appendChild(h1);
+                card.appendChild(p);
+            });
+        } else {
+            const errorMessage = document.createElement('marquee');
+            errorMessage.textContent = `Gah, it's not working!`;
+            app.appendChild(errorMessage);
+        }
+    }
+
+    request.send();
+
+}
+baseballAPI();
---
+++ js/scripts2.js
@@ -0,0 +1,144 @@
+const app = document.getElementById('root');
+const logo = document.createElement('img');
+logo.src = 'images/logo.png';
+
+const container = document.createElement('div');
+container.setAttribute('class', 'container2');
+
+app.appendChild(logo);
+
+const message = document.createElement('h1');
+message.textContent = "Salary Calculator";
+app.appendChild(message);
+
+app.appendChild(container);
+
+
+const message2 = document.createElement('p');
+message2.innerHTML = "<h4>Choose Amount: </h4><input type='radio' name='val' value='20' checked = 'checked'> 20%</input><br /> <input type='radio' name='val' value='30'>30%</input><br /><input type='radio' name='val' value='split'> 20/30 </input><br /><input type='radio' name='val' value='15split'> 15/30 </input>";
+container.appendChild(message2);
+
+
+var tableShowing = false;
+const button = document.createElement('button');
+button.textContent = "Calculate";
+button.addEventListener("click", function () {
+    if (tableShowing == false)
+        calcSalaries();
+    else {
+        var element = document.getElementById("salaryTable");
+        element.parentNode.removeChild(element);
+        calcSalaries();
+    }
+    tableShowing = true;
+})
+container.appendChild(button);
+
+
+
+
+function calcSalaries() {
+
+    var x = [];
+    var i;
+    for (i = 0; i < 50; i++) {
+        x[i] = i + 1;
+    }
+    var radios = document.getElementsByName("val")
+    var valSelected;
+    for (var i = 0, length = radios.length; i < length; i++) {
+        if (radios[i].checked) {
+            // do whatever you want with the checked radio
+            valSelected = (radios[i].value);
+
+            // only one radio can be logically checked, don't check the rest
+            break;
+        }
+    }
+
+    const box = document.createElement('table');
+    box.setAttribute("id", "salaryTable");
+    var tableFill = "<tr><td class ='bold'>Year One</td><td class ='bold'>Year Two</td><td class ='bold'>Year Three</td><td class ='bold'>Year Four</td><td class ='bold'>Year Five</td><td class ='bold'>Year Six</td><td class ='bold'>Year Seven</td><td class ='bold'>Year Eight</td><td class ='bold'>Year Nine</td><td class ='bold'>Year Ten</td></tr>";
+    for (i = 0; i < 50; i++) {
+        var j = x[i];
+
+
+        var cells;
+        if (valSelected == "20") {
+            cells = twentyPercent(j);
+        }
+        else if(valSelected == "30"){
+
+         cells = thirtyPercent(j);
+        }
+          else if(valSelected == "15split"){
+
+         cells = fifteenAndThirty(j);
+        }
+        else {
+            cells = twentyAndThirtyPercent(j);
+        }
+        tableFill = tableFill + "<tr>" + cells + "</tr>"
+        // twentyAndThirtyPercent(j);
+    }
+    box.innerHTML = tableFill;
+    app.appendChild(box);
+
+
+}
+
+
+function twentyPercent(x) {
+    var curr = x;
+    var result = "<td class='bold'>" + x + "</td>";
+    var i;
+    for (i = 0; i < 9; i++) {
+        curr = eval(Math.ceil(.20 * curr) + curr);
+        result = result + "<td>" + curr + "</td>";
+    }
+    return result;
+}
+
+function thirtyPercent(x) {
+    var curr = x;
+    var result = "<td class='bold'>" + x + "</td>";
+    var i;
+    for (i = 0; i < 9; i++) {
+        curr = eval(Math.ceil(.30 * curr) + curr);
+        result = result + "<td>" + curr + "</td>";
+    }
+    return result;
+}
+
+function twentyAndThirtyPercent(x) {
+    var curr = x;
+    var i;
+    var result = "<td class='bold'>" + x + "</td>";
+
+    for (i = 0; i < 4; i++) {
+        curr = eval(Math.ceil(.20 * curr) + curr);
+        result = result + "<td>" + curr + "</td>";
+    }
+    for (i = 4; i < 9; i++) {
+        curr = eval(Math.ceil(.30 * curr) + curr);
+        result = result + "<td>" + curr + "</td>";
+    }
+    return result;
+}
+
+
+function fifteenAndThirty(x) {
+    var curr = x;
+    var i;
+    var result = "<td class='bold'>" + x + "</td>";
+
+    for (i = 0; i < 4; i++) {
+        curr = eval(Math.ceil(.15 * curr) + curr);
+        result = result + "<td>" + curr + "</td>";
+    }
+    for (i = 4; i < 9; i++) {
+        curr = eval(Math.ceil(.30 * curr) + curr);
+        result = result + "<td>" + curr + "</td>";
+    }
+    return result;
+}
